cc4463a5fb162ca6307d5c1d39d13cdf08af2a4b
rajdev12 <rjnath98@gmail.com>
2024-05-10 15:29:11 +0530

    deep dive into data types

---
+++ 02-data-Types.js
@@ -0,0 +1,21 @@
+let a=2;
+let b="";
+let loggedIn='True';
+let marks=null;
+let age;
+//  alert("this is a alert") //This only interpreted by browser
+
+//DataTypes
+// 1. Number = 2 to the power 53
+//2.  bigint
+//3.String
+//4.boolean
+
+//5.null =standalone value
+//6. undefined
+
+// Objects are also a datatpe
+console.log("hi")
+
+console.log(typeof a)
+console.log(typeof null)
