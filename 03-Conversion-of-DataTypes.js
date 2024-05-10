71ab18228d06f73d723d96b427633e64d6d06149
rajdev12 <rjnath98@gmail.com>
2024-05-10 16:33:57 +0530

    boolean to string

--- 03-Conversion-of-DataTypes.js
+++ 03-Conversion-of-DataTypes.js
@@ -1,5 +1,6 @@
 let score = 33;
 console.log(typeof score); //number
+console.log(typeof(String(score)))  //string  
 
 let score2 = "33";
 console.log(typeof score2); //String
@@ -17,5 +18,31 @@ console.log(typeof Number(score3));  //number => it  will convert to number but 
 console.log(Number(score3));    //NaN  i.e Not a Number
 
 let score4 = null;
-console.log(typeof Number(score3));  //number => it  will convert to number but value will be NaN. 
-console.log(Number(score3));    //NaN  i.e Not a Number
+console.log(typeof Number(score4));  //number => it  will convert to number but value will be NaN. 
+console.log(Number(score4));    //NaN  i.e Not a Number
+
+let score5 = undefined;
+console.log(typeof Number(score5));  //number => it  will convert to number but value will be NaN. 
+console.log(Number(score5));
+
+let life=1;
+life=Boolean(life)
+console.log(life)   //true
+console.log(typeof life)   //boolean
+
+let life1="";
+life1=Boolean(life1)
+console.log(life1)   //false
+console.log(typeof life1)   //boolean
+
+
+let life2="anything";
+life2=Boolean(life2)
+console.log(life2)   //true
+console.log(typeof life2)   //boolean
+
+
+//Real Life Use Cases
+//Assume we are working with weather App, if some temperature data is coming from user, 
+//then we must check  for the type, cuz null and 0 degree celcius is two different thing
+
