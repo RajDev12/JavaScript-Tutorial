f368fdf4a0ab75b77e918776d699ef9541739a00
RajDev12 <rjnath98@gmail.com>
2024-05-15 17:08:44 +0530

    null & 0

---
+++ 05-comparasion-operators.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2<1);
+// console.log("2"<1);  //F
+// console.log("2">1);   //T
+
+// console.log(2==2);
+// console.log("2"==2);    //True =Checks Only value
+// console.log("2"===2);   //False   =Checks data types also i.e string !=number
+
+// console.log(null >0);
+// console.log(null <0);
+// console.log(null ==0);
+
+// //in comparasion, NUll is treated as 0.
+
+// console.log(null >=0);
+// console.log(null <=0);
+
+// console.log(undefined ==0);
+// console.log(undefined >=0);
+// console.log(undefined <=0);
