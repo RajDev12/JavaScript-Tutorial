e8b6f608a39108ca2c001a9932408e172c026ffb
RajDev12 <rjnath98@gmail.com>
2024-05-15 17:08:15 +0530

    diff ==&===

--- 04-Operations.js
+++ 04-Operations.js
@@ -1,3 +1,22 @@
 let a=3;
 let neg_a=-a;
 console.log(neg_a)
+
+let str1="hello ";
+console.log(str1.trim().length);
+let str2=" Developer"; 
+let str3=str1+str2;
+console.log(str3);
+
+
+console.log("1"+2)    //12
+console.log("1"+2+2)    //122
+console.log(3+"1"+2)    //312
+console.log(3+2+"1")    //51
+
+let counter=0;
+console.log(counter);
+++counter;
+console.log(counter);
+counter++;
+console.log(counter);
