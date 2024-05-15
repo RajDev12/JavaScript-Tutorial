a97bf86b14f97e100857815e80051362db9b3c36
RajDev12 <rjnath98@gmail.com>
2024-05-15 17:06:03 +0530

    refernce data type

--- 02-data-Types.js
+++ 02-data-Types.js
@@ -6,14 +6,19 @@ let age;
 //  alert("this is a alert") //This only interpreted by browser
 
 //DataTypes
+//Primitive  =Pass by value i.e a copy of that variable is passed, any changes doesn't effect the original one
 // 1. Number = 2 to the power 53
 //2.  bigint
 //3.String
 //4.boolean
 
-//5.null =standalone value
+//5.null =standalone value/empty nnot 0
 //6. undefined
+//7.Symbol  = uniquely define variable
 
+//Non-Primitive / Reference type = address is passed , any changes via new variable will reflect to original one also
+//Arrays
+//Functions
 // Objects are also a datatpe
 console.log("hi")
 
