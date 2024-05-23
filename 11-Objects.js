5dbc0800250bbdb9f5c71a84bfb68115462787bc
RajDev12 <rjnath98@gmail.com>
2024-05-23 17:12:44 +0530

    passing function inside object

--- 11-Objects.js
+++ 11-Objects.js
@@ -1,12 +1,12 @@
 // Singleton objects= Only one instance of the class can be possible. 
 // FOr eg= Single databse object is used throughout the program
 //Object literals
-const users={
+const users = {
     name: "Dev",
     age: 23,
-    email:"rjnath98@gmail.com",
+    email: "rjnath98@gmail.com",
     isAdult: false,
-    loggedInDays: ["Mon","tue","thu"]
+    loggedInDays: ["Mon", "tue", "thu"]
 }
 // //Accessing the objects
 // console.log(users.name);
@@ -15,21 +15,35 @@ const users={
 
 //Defining a symbol and use as key inside object
 const mySym = Symbol("key123")
-const users1={
+const users1 = {
     name: "Dev",
-    [mySym]:"key2333",
+    [mySym]: "key2333",
     age: 23,
-    email:"rjnath98@gmail.com",
+    email: "rjnath98@gmail.com",
     isAdult: false,
-    loggedInDays: ["Mon","tue","thu"]
+    loggedInDays: ["Mon", "tue", "thu"]
 }
-console.log(users1[mySym])
-console.log(typeof users1[mySym])
+// console.log(users1)
+// console.log(users1[mySym])
+// console.log(typeof users1[mySym])
 
-//CHanging the values from outside the objects
-users1.age=25;
-console.log(users1.age)
-//Freeze the object by making user1 immutable
-Object.freeze(users1)
-users1.age=99;
-console.log(users1.age);
+// //CHanging the values from outside the objects
+// users1.age=25;
+// console.log(users1.age)
+// //Freeze the object by making user1 immutable
+// Object.freeze(users1)
+// users1.age=99;
+// console.log(users1.age);
+
+
+//Adding a Function as a value to a key inside an object
+users1.greeting = function(){
+    console.log("Helo Developer")
+}
+console.log(users1.greeting())   //discuss undefned  later=one extra execution happen automatically
+console.log(users1.greeting)
+
+users1.greeting2 = function(){
+    console.log(`Helo Developer,${this.age}`)
+}
+console.log(users1.greeting2())
