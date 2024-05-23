1114bba57f4a6c228f773251a65558dc150476a0
RajDev12 <rjnath98@gmail.com>
2024-05-23 12:58:57 +0530

    Freezing the obj

--- 11-Objects.js
+++ 11-Objects.js
@@ -8,9 +8,28 @@ const users={
     isAdult: false,
     loggedInDays: ["Mon","tue","thu"]
 }
-//Accessing the objects
-console.log(users.name);
-// console.log(users[name])    //Error=expecting stirng argument
-console.log(users["name"])    //pass as string
+// //Accessing the objects
+// console.log(users.name);
+// // console.log(users[name])    //Error=expecting stirng argument
+// console.log(users["name"])    //pass as string
 
-//
+//Defining a symbol and use as key inside object
+const mySym = Symbol("key123")
+const users1={
+    name: "Dev",
+    [mySym]:"key2333",
+    age: 23,
+    email:"rjnath98@gmail.com",
+    isAdult: false,
+    loggedInDays: ["Mon","tue","thu"]
+}
+console.log(users1[mySym])
+console.log(typeof users1[mySym])
+
+//CHanging the values from outside the objects
+users1.age=25;
+console.log(users1.age)
+//Freeze the object by making user1 immutable
+Object.freeze(users1)
+users1.age=99;
+console.log(users1.age);
