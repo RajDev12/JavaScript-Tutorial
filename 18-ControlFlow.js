6c66b13810ab8c9e42981419ded7e841e0bebd70
RajDev12 <rjnath98@gmail.com>
2024-05-27 16:34:29 +0530

    switch

--- 18-ControlFlow.js
+++ 18-ControlFlow.js
@@ -50,3 +50,34 @@ if (userLoggedIn && debitCard && 2==3) {
 if (loggedInFromGoogle || loggedInFromEmail) {
     console.log("User logged in");
 }
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
