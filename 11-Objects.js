87c55c21c24cd793b3294f1f9f3ea0334988b723
RajDev12 <rjnath98@gmail.com>
2024-05-27 10:58:17 +0530

    spread of object

--- 11-Objects.js
+++ 11-Objects.js
@@ -13,6 +13,14 @@ const users = {
 // // console.log(users[name])    //Error=expecting stirng argument
 // console.log(users["name"])    //pass as string
 
+//Accessing the keys, values and entries
+// console.log(Object.keys(users)) //['name', 'age', 'email', 'isAdult', 'loggedInDays']   =array output
+// console.log(Object.values(users)) //['Dev', 23, 'rjnath98@gmail.com', false, ['Mon', 'tue', 'thu']] 
+// console.log(Object.entries(users)) //[['name', 'Dev'], ['age', 23], ['email', 'rjnath98@gmail.com'], ['isAdult', false], ['loggedInDays', ['Mon', 'tue', 'thu']]];
+// console.log(users.hasOwnProperty("name"))
+
+
+
 //Defining a symbol and use as key inside object
 const mySym = Symbol("key123")
 const users1 = {
@@ -37,13 +45,52 @@ const users1 = {
 
 
 //Adding a Function as a value to a key inside an object
-users1.greeting = function(){
-    console.log("Helo Developer")
-}
-console.log(users1.greeting())   //discuss undefned  later=one extra execution happen automatically
-console.log(users1.greeting)
+// users1.greeting = function(){
+//     console.log("Helo Developer")
+// }
+// console.log(users1.greeting())   //discuss undefned  later=one extra execution happen automatically
+// console.log(users1.greeting)
 
-users1.greeting2 = function(){
-    console.log(`Helo Developer,${this.age}`)
+// users1.greeting2 = function(){
+//     console.log(`Helo Developer,${this.age}`)
+// }
+// console.log(users1.greeting2())
+
+
+
+//==================================//////
+
+const appUser = new Object();   //Singleton object
+//OR  const appUser= {};    NON-singleton object
+appUser.id = "1234rtw";
+appUser.name = "Dev";
+appUser.isLoggedIn = false;
+
+// console.log(appUser);
+
+const app2user = {
+    email: "rjnath98@ibmc.com",
+    id: "1234rtw",
+    fullname: {
+        userFullName: {
+            firstName: "Dev",
+            seconName: "nath"
+        }
+    }
 }
-console.log(users1.greeting2())
+
+// cons`ole.log(app2user)
+// console.log(app2user.fullname.userFullName.seconName);
+// console.log(app2user.email)
+
+const obj1 = { 1: "a", 2: "b" }
+const obj2 = { 3: "a", 4: "b" }
+const obj3 = { obj1, obj2 }
+// console.log(obj3);  //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+
+// const obj4={ ...obj1, ...obj2}  //
+// console.log(obj4); 
+
+const obj5 = Object.assign({}, obj1,obj2)  //assign(target, source1,src2,src3)
+// console.log(obj5);
+
