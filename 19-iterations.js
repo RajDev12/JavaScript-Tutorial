ecdd09881b9b5947d4219ff3f030e99b3e933181
RajDev12 <rjnath98@gmail.com>
2024-05-28 14:42:20 +0530

    for of, for in

--- 19-iterations.js
+++ 19-iterations.js
@@ -10,7 +10,6 @@ for (let i = 1; i <= 10; i++) {
     //console.log(`Inner loop value ${j} and inner loop ${i}`);
     //console.log(i + '*' + j + ' = ' + i*j );
    }
-    
 }
 let myArray = ["flash", "batman", "superman"]
 //console.log(myArray.length);
@@ -41,4 +40,44 @@ for (let index = 1; index <= 20; index++) {
     }
    console.log(`Value of i is ${index}`);
    //continue skips that iteration and doesnt execute the statements below but directlty go for incrementing 
+}
+
+//while loop
+
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray1 = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray1.length) {
+    //console.log(`Value is ${myArray1[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+//For -Of loop
+const arr1=[1,2,3,4,5]
+
+for(const i of arr1){
+    console.log("for-of",i);
+}
+const greetings="Hello WQold!"
+
+for(const greet of greetings){
+    if(greet==" "){
+        continue;
+    }
+    console.log("Each char is:",greet);
 }
