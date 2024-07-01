0500a94134f6e595fa4aefc085ed4b090febf617
RajDev12 <rjnath98@gmail.com>
2024-07-01 15:33:08 +0530

    nested obj

--- 11-Objects.js
+++ 11-Objects.js
@@ -1,13 +1,33 @@
 // Singleton objects= Only one instance of the class can be possible. 
 // FOr eg= Single databse object is used throughout the program
-//Object literals
-const users = {
-    name: "Dev",
-    age: 23,
-    email: "rjnath98@gmail.com",
-    isAdult: false,
-    loggedInDays: ["Mon", "tue", "thu"]
-}
+// //Object literals
+// const users = {
+//     u_name: "Dev",  //entries
+//     age: 23,
+//     email: "rjnath98@gmail.com",
+//     isAdult: false,
+//     loggedInDays: ["Mon", "tue", "thu", "feb"]
+// }
+
+// users.email = "Mridhu@gmail.com"
+// console.log(users.email);
+
+// console.log(users.age);
+// users.loggedInDays[1] ="Sun"
+// console.log(users.loggedInDays[1]);
+// console.log(users.loggedInDays.length);
+// console.log(users.loggedInDays.slice(1));
+// console.log(users.loggedInDays.splice(2));
+// console.log(users.loggedInDays)
+// console.log(users);
+
+// console.log(users["u_name"]);
+// let a=Object.entries(users)
+// console.log(a[2][1]);
+
+
+
+
 // //Accessing the objects
 // console.log(users.name);
 // // console.log(users[name])    //Error=expecting stirng argument
@@ -17,16 +37,22 @@ const users = {
 // console.log(Object.keys(users)) //['name', 'age', 'email', 'isAdult', 'loggedInDays']   =array output
 // console.log(Object.values(users)) //['Dev', 23, 'rjnath98@gmail.com', false, ['Mon', 'tue', 'thu']] 
 // console.log(Object.entries(users)) //[['name', 'Dev'], ['age', 23], ['email', 'rjnath98@gmail.com'], ['isAdult', false], ['loggedInDays', ['Mon', 'tue', 'thu']]];
-// console.log(users.hasOwnProperty("name"))
+
+
+// console.log(users.hasOwnProperty("u_name"))
 
 
 
 //Defining a symbol and use as key inside object
-const mySym = Symbol("key123")
+const mySym = Symbol()
+function age(){
+  let age =23
+  return age
+}
 const users1 = {
     name: "Dev",
     [mySym]: "key2333",
-    age: 23,
+    age: age(),
     email: "rjnath98@gmail.com",
     isAdult: false,
     loggedInDays: ["Mon", "tue", "thu"]
@@ -39,9 +65,10 @@ const users1 = {
 // users1.age=25;
 // console.log(users1.age)
 // //Freeze the object by making user1 immutable
-// Object.freeze(users1)
-// users1.age=99;
+Object.freeze(users1)
+users1.age=99;
 // console.log(users1.age);
+// console.log(users1);
 
 
 //Adding a Function as a value to a key inside an object
@@ -83,14 +110,14 @@ const app2user = {
 // console.log(app2user.fullname.userFullName.seconName);
 // console.log(app2user.email)
 
-const obj1 = { 1: "a", 2: "b" }
+const obj1 = { 1: "a",2: "b" }
 const obj2 = { 3: "a", 4: "b" }
 const obj3 = { obj1, obj2 }
-// console.log(obj3);  //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+console.log(obj3);  //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
 
-// const obj4={ ...obj1, ...obj2}  //
-// console.log(obj4); 
+const obj4={ ...obj1, ...obj2}  //
+console.log(obj4); 
 
 const obj5 = Object.assign({}, obj1,obj2)  //assign(target, source1,src2,src3)
-// console.log(obj5);
+console.log(obj5);
 
