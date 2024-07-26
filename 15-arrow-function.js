a58155226798c1bba7e4f9a083aa6dd41fb944f4
RajDev12 <rjnath98@gmail.com>
2024-07-26 17:43:04 +0530

    return

--- 15-arrow-function.js
+++ 15-arrow-function.js
@@ -7,6 +7,7 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
+        console.log("hi");
         console.log(this);  //current object
     }
 
@@ -19,10 +20,10 @@ const user = {
 // console.log(this);  => {}  i.e empty object/global object
 //inside browser it will be window object and in nodejs it will be global object
 
-// function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);  //undefined => cant use this inside a function
-// }
+function chai(){
+    let username = "hitesh"
+    console.log(this.username);  //undefined => cant use this inside a function
+}
 
 // chai()
 
@@ -31,28 +32,42 @@ const user = {
 //     console.log(this.username);  //unefined
 // }
 
-const chai =  () => {
-    let username = "hitesh"
-    console.log(this);  //can use this but will give empty obejct
-}
+// const chai =  () => {
+//     let username = "hitesh"
+//     console.log(this);  //can use this but will give empty obejct
+// }
 
 
 // chai()
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
 
 // const addTwo = (num1, num2) =>  num1 + num2   //single statement
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
-const addTwo = (num1, num2) => ({username: "hitesh"})  //returning object
+// const addTwo = (num1, num2) => ({username: "hitesh"})  //returning object
 
 
-console.log(addTwo(3, 4))
+// console.log(addTwo(3, 4))
 
 
-// const myArray = [2, 5, 3, 7, 8]
+const myArray = [2, 5, 3, 2, 2,2, 7, 8]
+
+function occurence(arr, target ){
+    let counter=0
+    for(i=0;i<arr.length;i++){
+        if(arr[i]==target){  
+            counter++;
+        }
+       
+    }
+    return counter;
+}
+   
+console.log(occurence(myArray, 2));
 
 // myArray.forEach()
+
