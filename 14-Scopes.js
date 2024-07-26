71f89d61977d13b5b3bebfb846958ff6d476cb3c
RajDev12 <rjnath98@gmail.com>
2024-07-26 17:42:39 +0530

    block Scope

--- 14-Scopes.js
+++ 14-Scopes.js
@@ -1,15 +1,15 @@
 let A= 100;  //Global scope = can be accessed from anywhere in this file
 
-if (true) {  // Block Scope = can be accessed only within this block
+if (true) {  // Block Scope = can be accessed only withiqn this block
     let A = 10;
     const b = 20;
     var c = 30; // Exception
     console.log("inner A:",A)
 }
 //     {   } =Scope
-
+  
 console.log("outer A:",A); 
-// console.log(b);
+// console.log(b);  // cant access the block scope, cuz the b is inside the if block
 console.log(c); // can be accessed outside the block scope , so avoid using it
 
 
