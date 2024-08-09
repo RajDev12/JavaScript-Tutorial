d7d9ad47b1f5a898806256e97180460ee17fd5f8
RajDev12 <rjnath98@gmail.com>
2024-08-09 16:28:30 +0530

    iife

--- 16-IIFE.js
+++ 16-IIFE.js
@@ -4,14 +4,35 @@
 //    ()()  
 //  (function defination)(calling);  //; is mandotory
 //named iife
-(function chai(){
-    console.log("I am chai");
-})();
+// (function chai(){
+//     console.log("I am chai")
+// })();
  
 
-//unnamed iife
-(  (name) => {
-    console.log(`Automatically ${name} am executed`);
-} ) ("dev");
+// //unnamed iife
+// (  (name) => {
+//     console.log(`Automatically ${name} am executed`);
+// } ) ("dev");
+
+
+
+
+// function defination
+// function cofee(a,b){
+//     console.log(a+b);  
+// }
+// cofee('a','b');  //function calling
+
+
+// (function cofee(a,b){
+//     console.log(a+b);  
+// })(2,3)
+
+((a,b)=>{
+   //function body
+    console.log(a+b); 
+}) (5,-5)
+
+
 
 
