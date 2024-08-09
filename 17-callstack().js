8dcc98dc5955d1220005726eb3117f604c465acf
RajDev12 <rjnath98@gmail.com>
2024-08-09 16:28:20 +0530

    stack

--- 17-callstack().js
+++ 17-callstack().js
@@ -3,3 +3,17 @@
 //This worked on the principal of LIFO
 //Last In First Out
 
+function add(a,b){
+    console.log(a+b);
+    
+}
+function sub(a,b){
+    console.log(a-b);
+    
+}
+function multiple(a,b){
+    console.log(a*b);
+}
+multiple(2,6)
+add(2,6)
+sub(10,4)
